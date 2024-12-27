45c9e1ed47a289e3ecf5693c752daa3520ef311c
subhekshya Baral <baralshubu@gmail.com>
2024-12-27 15:36:05 +0545

    addimg login selectors

--- cypress/e2e/infopractice.cy.js
+++ cypress/e2e/infopractice.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://cqweb.info.com.np/')
-  })
-})
+import { loginSelectors } from "../support/selectors/loginselectors";
+describe('login form of cqnoval', () => {
+
+  it('should log in successfully', () => {
+
+    // Visit the URL
+    cy.visit('https://cqweb.info.com.np/');
+  
+    // Input username and password
+    cy.get (loginSelectors.userNameSelector).clear().type('tatsam');
+    cy.get (loginSelectors.passwordSelector).clear().type('Iphone123!@#');
+    cy.get(loginSelectors.loginButtonSelector).should('contain.text', 'Log In').click();
+
+    cy.wait(500); // Wait for 500ms
+
+    // Assert the correct URL is loaded
+    cy.url().should('include', 'https://cqweb.info.com.np/#/dashBoard');
+  });
+});
+
+  
---
+++ cypress/support/selectors/loginSelectors.js
@@ -0,0 +1,6 @@
+export const loginSelectors = { 
+    //login selectors
+    userNameSelector : '#basic_username' ,
+    passwordSelector : '#basic_password' ,
+    loginButtonSelector : 'button[type="submit"]' ,
+}
